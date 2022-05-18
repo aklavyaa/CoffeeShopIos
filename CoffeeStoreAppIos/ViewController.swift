03d95fd91624f71d647537eef4b28b133ed37b94
Z3N00 <78555919+Gurwindersingh85@users.noreply.github.com>
2022-05-18 15:27:09 -0400

    Update ViewController.swift
    created and updated view controller for create account

--- CoffeeStoreAppIos/ViewController.swift
+++ CoffeeStoreAppIos/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CoffeeStoreAppIos
 //
-//  Created by admin user on 2022-04-14.
+//  Created by Gurwinder on 2022-04-14.
 //  Copyright © 2022 admin user. All rights reserved.
 //
 
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
     @IBOutlet weak var userName: UITextField!
     
+   
     @IBOutlet weak var password: UITextField!
     
+   
     @IBAction func signUpClick(_ sender: UIButton) {
+        
+        
+        
+        
+        
+        
         
          let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "CreateAccountVC") as? CreateAccountVC
                             
@@ -24,17 +33,45 @@ class ViewController: UIViewController {
     @IBAction func loginCalled(_ sender: UIButton) {
          if userName.text == "admin" && password.text == "123"{
          let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "AdminVCViewController") as? AdminVCViewController
+            
+            
         
             self.navigationController?.pushViewController(vc!, animated: true)
 
 
-               } else {
-                        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "InventoryViewController") as? InventoryViewController
-                     
-                         self.navigationController?.pushViewController(vc!, animated: true)
                }
+            
+        // else if userName.text == StoreRes.userName && password.text == StoreRes.password {
+            
+            
+          //                  let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "InventoryViewController") as? InventoryViewController
+                         
+            //                 self.navigationController?.pushViewController(vc!, animated: true)
+            
+            
+         //}
+         
+         
+         
+         
+         
+         
+         
+         
+         else {
+               
+        
+                       
+                              let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "InventoryViewController") as? InventoryViewController
+                           
+                               self.navigationController?.pushViewController(vc!, animated: true)
+            
+            
+        }
         
     }
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
