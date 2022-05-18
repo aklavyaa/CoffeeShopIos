8e13a79f54f2d164199edec47060b9f4cb7ace5e
Z3N00 <78555919+Gurwindersingh85@users.noreply.github.com>
2022-05-18 13:49:15 -0400

    Create ThankYou.swift
    updated thank you screen

---
+++ CoffeeStoreAppIos/ThankYou.swift
@@ -0,0 +1,21 @@
+//
+//  ThankYou.swift
+//  CoffeeStoreAppIos
+//
+//  Created by admin user on 2022-05-10.
+//  Copyright © 2022 admin user. All rights reserved.
+// This screen is created by Gurwinder
+
+import Foundation
+
+import UIKit
+
+class ThankYou: UIViewController{
+    
+    @IBAction func homeClikc(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        
+    }
+    
+}
