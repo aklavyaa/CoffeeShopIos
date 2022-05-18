c98f64c90fa8e728996eaaa046962a66c4aab5a8
adnan1608 <88289321+adnan1608@users.noreply.github.com>
2022-05-17 21:16:59 -0400

    Middle View Cell

--- CoffeeStoreAppIos/MiddelViewCellTableViewCell.swift
+++ CoffeeStoreAppIos/MiddelViewCellTableViewCell.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class MiddelViewCellTableViewCell: UITableViewCell,UICollectionViewDataSource,
 UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+    
+    
+    var parent =  UIViewController()
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return StoreRes.catList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
          let cell = horizontalCollectionList.dequeueReusableCell(withReuseIdentifier: "CatMiddleViewCell", for: indexPath) as! CatMiddleViewCell
                
+        
+        cell.catName.text = StoreRes.catList[indexPath.row].catName
+        
+        
+        cell.totalMenuItems.text = "Total items \(StoreRes.catList[indexPath.row].totalProductItems)"
+        
+        
+        
+        cell.cellIconImage.image = UIImage(named: StoreRes.catList[indexPath.row].catLogo)
         
                return cell
         
@@ -31,11 +44,15 @@ UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
    
         
         
-        
-        let invController = InventoryViewController().storyboard?.instantiateViewController(withIdentifier : "InventoryViewController") as? InventoryViewController
+     //   let invController = InventoryViewController().storyboard?.instantiateViewController(withIdentifier : "InventoryViewController") as? InventoryViewController
 
-        invController?.navigationController?.pushViewController(ProductListViewController(nibName: "ProductListViewController", bundle: nil), animated: true)
+     //   invController?.navigationController?.pushViewController(ProductListViewController(nibName: "ProductListViewController", bundle: nil), animated: true)
         
+         let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ProductListViewController") as? ProductListViewController
+        
+        vc!.catId = StoreRes.catList[indexPath.row].catId
+       parent.navigationController?.pushViewController(vc!, animated: true)
+        print("Hello")
         
     }
     
