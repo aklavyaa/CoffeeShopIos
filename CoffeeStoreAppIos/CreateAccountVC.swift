f54706c5ad3c1d79a3fbd4d7e8c8867de5f7a559
adnan1608 <88289321+adnan1608@users.noreply.github.com>
2022-05-17 21:12:05 -0400

    Creating Account

--- CoffeeStoreAppIos/CreateAccountVC.swift
+++ CoffeeStoreAppIos/CreateAccountVC.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
-   
+    @IBOutlet var password: UITextField!
+    @IBOutlet var email: UITextField!
+    @IBOutlet var username: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
-
+    @IBAction func createAccount(_ sender: UIButton) {
+        
+        if password.text!.isEmpty || email.text!.isEmpty || username.text!.isEmpty {
+            
+            
+            
+        } else {
+            
+            StoreRes.userName = username.text!
+            
+            StoreRes.password = password.text!
+            
+            StoreRes.email = email.text!
+            
+            
+              let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ViewController") as? ViewController
+                               
+                                   self.navigationController?.pushViewController(vc!, animated: true)
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
